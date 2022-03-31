b53f63fb40dc8487b477da7ec3ff54909e54fd19
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-31 14:49:22 +0200

    add jsnack_3

---
+++ jsnack_3/assets/js/main.js
@@ -0,0 +1,21 @@
+const first_name = [`antonio`, `fabio`, `carmine`, `osvaldo`];
+const last_name = [` frizzi`, ` pitti`, ` ceri`, ` quasi`];
+
+let full_name;
+
+let fake_list = [];
+
+//generare una lista di nomi e cognomi
+
+let choice = prompt("vuoi aggiungere un nome e cognome alla fake list? [y/n]")
+
+while (choice == `y`) {
+    
+    full_name = first_name[Math.ceil(Math.random() * 3)] + last_name[Math.ceil(Math.random() * 3)];
+    
+    fake_list.push(full_name);
+    console.log(fake_list);
+
+    choice = prompt("vuoi aggiungerne un'altro? [y/n]")
+}
+
