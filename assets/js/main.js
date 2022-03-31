19370b210ce4fe712aa5ffd6a377bec6acea1b27
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-31 12:47:39 +0200

    add while cicle solution

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,26 @@ sum = 0;
 
 //ciclo for
 
-for (let i = 0; i < 5; i++) {
+/* for (let i = 0; i < 5; i++) {
+    
+
+    sum = sum + num;
+}
+
+console.log(sum); */
+
+//ciclo while
+
+let i = 0;
+
+
+
+while (i < 5) {
     let num = parseInt(prompt("inserisci un numero"));
 
     sum = sum + num;
+
+    i++
 }
 
 console.log(sum);
